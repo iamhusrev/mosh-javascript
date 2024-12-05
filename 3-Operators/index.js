8ac89d5b5ebb473a79c159cf22cfdc8a45a250e0
nogayhusrev <nogayhusrev@gmail.com>
2024-12-05 12:55:21 +0300

    operators

---
+++ 3-Operators/index.js
@@ -0,0 +1,57 @@
+// ! Operators
+
+// 1. Arithmetic Operators
+let a = 10;
+let b = 20;
+
+console.log("1 ->", a + b); // 30
+console.log("2 ->", a - b); // -10
+console.log("3 ->", a * b); // 200
+console.log("4 ->", a / b); // 0.5
+console.log("5 ->", a % b); // 10
+console.log("6 ->", a ** b); // 100000000000000000000
+
+let c = 10;
+console.log("7 ->", c++); // 10
+console.log("8 ->", c); // 11
+console.log("9 ->", ++c); // 12
+console.log("10 ->", c); // 12
+console.log("11 ->", c--); // 12
+console.log("12 ->", c); // 11
+console.log("13 ->", --c); // 10
+console.log("14 ->", c); // 10
+
+// 2. Assignment Operators
+let x = 10;
+
+x += 5; // x = x + 5
+console.log("15 ->", x); // 15
+x -= 5; // x = x - 5
+console.log("16 ->", x); // 10
+x *= 5; // x = x * 5
+console.log("17 ->", x); // 50
+x /= 5; // x = x / 5
+console.log("18 ->", x); // 10
+x %= 5; // x = x % 5
+console.log("19 ->", x); // 0
+x **= 5; // x = x ** 5
+console.log("20 ->", x); // 0
+
+// 3. Comparison Operators
+let y = 10;
+let z = 20;
+console.log("21 ->", y == z); // false
+console.log("22 ->", y != z); // true
+console.log("23 ->", y > z); // false
+console.log("24 ->", y < z); // true
+console.log("25 ->", y >= z); // false
+console.log("26 ->", y <= z); // true
+console.log("27 ->", y === z); // false
+
+// 4. Logical Operators
+// 5. Bitwise Operators
+// 6. String Operators
+// 7. Conditional (Ternary) Operator
+// 8. Comma Operator
+// 9. Unary Operators
+// 10. Relational Operators
